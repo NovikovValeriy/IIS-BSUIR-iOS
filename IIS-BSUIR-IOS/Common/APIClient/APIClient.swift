cbfb7d017583fe567d5d476a45143816683a04f8
NovikovValeriy <novikov.valera2005@gmail.com>
2026-04-09 00:19:25 +0300

    Added initial APIClient and its components

---
+++ IIS-BSUIR-IOS/Common/APIClient/APIClient.swift
@@ -0,0 +1,114 @@
+//
+//  APIClient.swift
+//  IIS-BSUIR-IOS
+//
+//  Created by Valery Novikau on 9.04.26.
+//
+
+import Foundation
+
+protocol APIClient {
+    var baseURL: URL { get }
+    func send(request: URLRequest) async throws(APIError) -> APIResponse
+    func defaultHeaders(additionalHeaders: [String: String]) throws(APIError) -> [String: String]
+}
+
+extension APIClient {
+    func sendRequest<T: Decodable>(
+        path: String,
+        httpMethod: HTTPMethod,
+        queryParams: [String: String]? = nil,
+        body: APIRequestBody? = nil,
+        additionalHeaders: [String: String] = [:]
+    ) async throws(APIError) -> T {
+        var response: APIResponse?
+
+        let request = try self.buildRequest(
+            path: path,
+            method: httpMethod,
+            queryParams: queryParams,
+            body: body,
+            additionalHeaders: additionalHeaders
+        )
+
+        response = try? await self.send(request: request)
+
+        guard
+            let response,
+            (200...299).contains(response.statusCode)
+        else {
+            throw APIError.networkError(statusCode: response?.statusCode ?? 400)
+        }
+
+        let decodedResponse: T
+        do {
+             decodedResponse = try self.decodeData(response: response)
+        } catch {
+            throw APIError.parsingError(error)
+        }
+        return decodedResponse
+    }
+
+    private func buildRequest(
+        path: String,
+        method: HTTPMethod,
+        queryParams: [String: String]? = nil,
+        body: APIRequestBody? = nil,
+        additionalHeaders: [String: String] = [:],
+    ) throws(APIError) -> URLRequest {
+        let url = self.baseURL.appendingPathComponent(path)
+
+        guard var urlComponents = URLComponents(
+            url: url,
+            resolvingAgainstBaseURL: false
+        ) else {
+            throw APIError.invalidRequest
+        }
+
+        if let queryParams = queryParams {
+            urlComponents.queryItems = queryParams.map { URLQueryItem(name: $0.key, value: $0.value) }
+        }
+        guard let url = urlComponents.url else {
+            throw APIError.invalidRequest
+        }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = method.rawValue
+
+        var headers: [String: String] = [:]
+        do {
+            headers = try self.defaultHeaders(additionalHeaders: additionalHeaders)
+        } catch {
+            throw APIError.invalidRequest
+        }
+        request.allHTTPHeaderFields = headers
+
+        var contentType: String?
+        switch body {
+        case .jsonBody(let body):
+            let encoder = JSONEncoder()
+            encoder.keyEncodingStrategy = .convertToSnakeCase
+            request.httpBody = try? encoder.encode(body)
+            contentType = "application/json"
+        case .urlEncodedBody(let body):
+            var components = URLComponents()
+            components.queryItems = body.map { URLQueryItem(name: $0.key, value: $0.value) }
+            request.httpBody = components.query?.data(using: .utf8)
+            contentType = "application/x-www-form-urlencoded"
+        case .none:
+            break
+        }
+
+        if let contentType {
+            request.setValue(contentType, forHTTPHeaderField: "Content-Type")
+        }
+
+        return request
+    }
+
+    private func decodeData<T: Decodable>(response: APIResponse) throws -> T {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return try decoder.decode(T.self, from: response.data)
+    }
+}
---
+++ IIS-BSUIR-IOS/Common/APIClient/APIResponse.swift
@@ -0,0 +1,13 @@
+//
+//  APIResponse.swift
+//  IIS-BSUIR-IOS
+//
+//  Created by Valery Novikau on 9.04.26.
+//
+
+import Foundation
+
+struct APIResponse {
+    let statusCode: Int
+    let data: Data
+}
